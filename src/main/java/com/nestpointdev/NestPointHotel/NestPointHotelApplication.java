e6e4f249a2a51e7de94263dbb6d4850dc05def71
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-11-20 16:50:39 +0100

    Initial commit

---
+++ src/main/java/com/nestpointdev/NestPointHotel/NestPointHotelApplication.java
@@ -0,0 +1,13 @@
+package com.nestpointdev.NestPointHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestPointHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestPointHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestpointdev/NestPointHotel/NestPointHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestpointdev.NestPointHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestPointHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
