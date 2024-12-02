2ff668526f1f911526feb779c5ff4e9dabfa9a28
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-12-02 17:55:52 +0100

    fixed Spring Data JPA query, corrected the named parameters

--- src/main/java/com/nestpointdev/NestPointHotel/repositories/RoomRepository.java
+++ src/main/java/com/nestpointdev/NestPointHotel/repositories/RoomRepository.java
@@ -3,15 +3,28 @@ package com.nestpointdev.NestPointHotel.repositories;
 import com.nestpointdev.NestPointHotel.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-    @Query("SELECT DISTINCNT r.roomType FROM Room r")
+    @Query("SELECT DISTINCT r.roomType FROM Room r")
     List<String> findDistinctRoomTypes();
-    @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
+
+    @Query("SELECT r FROM Room r " +
+            "LEFT JOIN Booking b ON r.id = b.room.id " +
+            "WHERE b.id IS NULL")
     List<Room> getAllAvailableRooms();
-    @Query("SELECT r FROM r WHERE r.room.roomType LIKE %:roomType% AND r.id NOT IN (SELECT bk.room.id FROM Booking bk where (bk.checkInDate <= :bk.checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
-    List<Room> findAvailableRoomsByDateAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
+
+    @Query("SELECT r FROM Room r " +
+            "WHERE r.roomType LIKE %:roomType% " +
+            "AND r.id NOT IN (" +
+            "SELECT bk.room.id FROM Booking bk " +
+            "WHERE (bk.checkInDate <= :checkOutDate) " +
+            "AND (bk.checkOutDate >= :checkInDate))")
+    List<Room> findAvailableRoomsByDateAndTypes(
+            @Param("checkInDate") LocalDate checkInDate,
+            @Param("checkOutDate") LocalDate checkOutDate,
+            @Param("roomType") String roomType);
 }
