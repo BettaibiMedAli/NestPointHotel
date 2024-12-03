391584a874ace786c96e31b589b46c3aafa875d7
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-12-03 18:33:27 +0100

    added booking service interface

---
+++ src/main/java/com/nestpointdev/NestPointHotel/services/interfaces/IBookingService.java
@@ -0,0 +1,12 @@
+package com.nestpointdev.NestPointHotel.services.interfaces;
+
+import com.nestpointdev.NestPointHotel.dto.Response;
+import com.nestpointdev.NestPointHotel.models.Booking;
+
+public interface IBookingService {
+    Response saveBooking(Long roomId, Long userId, Booking bookingRequest);
+    Response findBookingByConfirmationCode(String ConfirmationCode);
+    Response getAllBookings();
+    Response cancelBooking();
+
+}
